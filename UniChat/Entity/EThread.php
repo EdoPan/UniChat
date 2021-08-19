927f808c5a6778b450fabf6275c391db232bf986
AnToN1o99 <antonio.diluca1999@gmail.com>
2021-08-19 14:53:31 +0200

    Aggiunta phpdoc, correzione metodi, costruttori e attributi

--- UniChat/Entity/EThread.php
+++ UniChat/Entity/EThread.php
@@ -3,211 +3,279 @@
     declare(strict_types = 1);
     require_once __DIR__ . "\..\utility.php";
 
-    class EThread
+/**
+ *
+ */
+class EThread
     {
 
-        private static string $formatoData = "Y-m-d H:i:s";
 
-        private int $id;
-        private string $titolo;
-        private string $testo;
-        private string $data;
-        private array $allegati;
-        private EUser $autoreThread;
-        private ECategoria $categoriaThread;
-        private array $tags;
-        private EValutazione $valutazione;
-        private array $risposte;
+    /**
+     * Constante che riporta il formato di data ed ora utlizzato per tenere traccia di quando è stato creato il thread.
+     */
+    const FORMATO_DATA = "Y-m-d H:i:s";
 
-        public function __construct(int $id, string $titolo, string $testo, ?array $allegati, EUser $autoreThread, ECategoria $categoriaThread, array $tags,
-                                    EValutazione $valutazione, ?array $risposte)
-        {
+    /**
+     * Identificativo del thread.
+     * @var int
+     */
+    private int $id;
 
+    /**
+     * Titolo del thread.
+     * @var string
+     */
+    private string $titolo;
+
+    /**
+     * Testo del thread.
+     * @var string
+     */
+    private string $testo;
+
+    /**
+     * Data e ora di creazione del thread.
+     * @var string|false
+     */
+    private string $data;
+
+    /**
+     * Allegati aggiuntivi del thread. Si tratta di un array contente array associativi che rappresentano il file
+     * allegato al thread. Ogni array associativo presenta i seguenti campi: l'identificativo del file, il nome del
+     * file, la dimensione del file, il tipo del file e il file stesso rappresentato in formato stringa.
+     * @var array
+     */
+    private array $allegati;
+
+    /**
+     * Autore del thread.
+     * @var EUser
+     */
+    private EUser $autoreThread;
+
+    /**
+     * Categoria di appartenenza del thread.
+     * @var ECategoria
+     */
+    private ECategoria $categoriaThread;
+
+    /**
+     * Valutazione del thread da parte di altri utenti.
+     * @var EValutazione
+     */
+    private EValutazione $valutazione;
+
+    /**
+     * Risposte al thread. Si tratta un array di ERisposta.
+     * @var array
+     */
+    private array $risposte;
+
+    /**
+     * Costruttore di EThread
+     * Se il thread è stato appena creato allora non ha un id e quindi viene passato come null e impostato a 0.
+     * Se il thread è appena stato creato allora non ha la data e l'ora di creazione, quindi viene passato come null e
+     * vengono impostate la data e l'ora.
+     * Se l'autore non ha aggiunto allegati allora viene passato null e impostato un array vuoto.
+     * Se il thread non ha ancora delle risposte allora viene passato null e impostato un array vuoto.
+     * @param int|null $id
+     * @param string $titolo
+     * @param string $testo
+     * @param string|null $data
+     * @param array|null $allegati
+     * @param EUser $autoreThread
+     * @param ECategoria $categoriaThread
+     * @param EValutazione $valutazione
+     * @param array|null $risposte
+     */
+    public function __construct(?int $id, string $titolo, string $testo, ?string $data, ?array $allegati, EUser $autoreThread,
+                                ECategoria $categoriaThread, EValutazione $valutazione, ?array $risposte)
+    {
+        if(isset($id)) {
             $this->id = $id;
-            $this->titolo = $titolo;
-            $this->testo = $testo;
-            $this->data = date(self::$formatoData);
-            if(isset($allegati)){
-                $this->allegati = $allegati;
-            } else {
-                $this->allegati = array();
-            }
-            $this->autoreThread = $autoreThread;
-            $this->categoriaThread = $categoriaThread;
-            $this->tags = $tags;
-            $this->valutazione = clone $valutazione;
-            if(isset($risposte)){
-                $this->risposte = $risposte;
-            } else {
-                $this->risposte = array();
-            }
+        } else {
+            $this->id = 0;
         }
-
-        /**
-         * @return int
-         */
-        public function getId(): int
-        {
-            return $this->id;
+        $this->titolo = $titolo;
+        $this->testo = $testo;
+        if(isset($data)){
+            $this->data = $data;
+        } else {
+            $this->data = date(self::FORMATO_DATA);
         }
-
-        /**
-         * @return string
-         */
-        public function getTitolo(): string
-        {
-            return $this->titolo;
-        }
-
-        /**
-         * @return string
-         */
-        public function getTesto(): string
-        {
-            return $this->testo;
-        }
-
-        /**
-         * @return false|string
-         */
-        public function getData()
-        {
-            return $this->data;
-        }
-
-        /**
-         * @return array
-         */
-        public function getAllegati(): array
-        {
-            return $this->allegati;
-        }
-
-        /**
-         * @return EUser
-         */
-        public function getAutoreThread(): EUser
-        {
-            return $this->autoreThread;
-        }
-
-        /**
-         * @return ECategoria
-         */
-        public function getCategoriaThread(): ECategoria
-        {
-            return $this->categoriaThread;
-        }
-
-        /**
-         * @return array
-         */
-        public function getTags(): array
-        {
-            return $this->tags;
-        }
-
-        /**
-         * @return EValutazione
-         */
-        public function getValutazione(): EValutazione
-        {
-            return clone $this->valutazione;
-        }
-
-        /**
-         * @return array
-         */
-        public function getRisposte(): array
-        {
-            return $this->risposte;
-        }
-
-        /**
-         * @param int $id
-         */
-        public function setId(int $id): void
-        {
-            $this->id = $id;
-        }
-
-        /**
-         * @param string $titolo
-         */
-        public function setTitolo(string $titolo): void
-        {
-            $this->titolo = $titolo;
-        }
-
-        /**
-         * @param string $testo
-         */
-        public function setTesto(string $testo): void
-        {
-            $this->testo = $testo;
-        }
-
-        /**
-         * @return void
-         */
-        public function setData(): void
-        {
-            $this->data = date(self::$formatoData);
-        }
-
-        /**
-         * @param array $allegati
-         */
-        public function setAllegati(array $allegati): void
-        {
+        if(isset($allegati)){
             $this->allegati = $allegati;
+        } else {
+            $this->allegati = array();
         }
-
-        /**
-         * @param EUser $autoreThread
-         */
-        public function setAutoreThread(EUser $autoreThread): void
-        {
-            $this->autoreThread = $autoreThread;
-        }
-
-        /**
-         * @param ECategoria $categoriaThread
-         */
-        public function setCategoriaThread(ECategoria $categoriaThread): void
-        {
-            $this->categoriaThread = $categoriaThread;
-        }
-
-        /**
-         * @param array $tags
-         */
-        public function setTags(array $tags): void
-        {
-            $this->tags = $tags;
-        }
-
-        /**
-         * @param EValutazione $valutazione
-         */
-        public function setValutazione(EValutazione $valutazione): void
-        {
-            $this->valutazione = clone $valutazione;
-        }
-
-        /**
-         * @param array $risposte
-         */
-        public function setRisposte(array $risposte): void
-        {
+        $this->autoreThread = $autoreThread;
+        $this->categoriaThread = $categoriaThread;
+        $this->valutazione = clone $valutazione;
+        if(isset($risposte)){
             $this->risposte = $risposte;
+        } else {
+            $this->risposte = array();
         }
+    }
 
-        /**
-         * @return int
-         */
-        public function countRisposte(): int
-        {
-            return count($this->risposte);
-        }
+    /**
+     * Restituisce l'identificativo del thread.
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * Restituisce il titolo del thread.
+     * @return string
+     */
+    public function getTitolo(): string
+    {
+        return $this->titolo;
+    }
+
+    /**
+     * Restituisce il testo del thread.
+     * @return string
+     */
+    public function getTesto(): string
+    {
+        return $this->testo;
+    }
+
+    /**
+     * Restituisce la data del thread.
+     * @return string
+     */
+    public function getData(): string
+    {
+        return $this->data;
+    }
+
+    /**
+     * Restituisce un array contenente array associativi che rapprensentano i file allegati.
+     * @return array
+     */
+    public function getAllegati(): array
+    {
+        return $this->allegati;
+    }
+
+    /**
+     * Restituisce l'autore del thread.
+     * @return EUser
+     */
+    public function getAutoreThread(): EUser
+    {
+        return $this->autoreThread;
+    }
+
+    /**
+     * Restituisce la categoria di appartenenza del thread.
+     * @return ECategoria
+     */
+    public function getCategoriaThread(): ECategoria
+    {
+        return $this->categoriaThread;
+    }
+
+    /**
+     * Restituisce la valutazione del thread.
+     * @return EValutazione
+     */
+    public function getValutazione(): EValutazione
+    {
+        return clone $this->valutazione;
+    }
+
+    /**
+     * Restituisce un array contenente le risposte al thread.
+     * @return array
+     */
+    public function getRisposte(): array
+    {
+        return $this->risposte;
+    }
+
+    /**
+     * Imposta l'identificativo del thread.
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * Imposta il titolo del thread.
+     * @param string $titolo
+     */
+    public function setTitolo(string $titolo): void
+    {
+        $this->titolo = $titolo;
+    }
+
+    /**
+     * Imposta il testo del thread.
+     * @param string $testo
+     */
+    public function setTesto(string $testo): void
+    {
+        $this->testo = $testo;
+    }
+
+    /**
+     * Imposta la data del thread alla data e l'ora attuale.
+     */
+    public function setData(): void
+    {
+        $this->data = date(self::FORMATO_DATA);
+    }
+
+    /**
+     * Imposta gli allegati del thread.
+     * @param array $allegati
+     */
+    public function setAllegati(array $allegati): void
+    {
+        $this->allegati = $allegati;
+    }
+
+    /**
+     * Imposta l'autore del thread.
+     * @param EUser $autoreThread
+     */
+    public function setAutoreThread(EUser $autoreThread): void
+    {
+        $this->autoreThread = $autoreThread;
+    }
+
+    /**
+     * Imposta la categoria di appartenenza del thread.
+     * @param ECategoria $categoriaThread
+     */
+    public function setCategoriaThread(ECategoria $categoriaThread): void
+    {
+        $this->categoriaThread = $categoriaThread;
+    }
+
+    /**
+     * Imposta la valutazione del thread.
+     * @param EValutazione $valutazione
+     */
+    public function setValutazione(EValutazione $valutazione): void
+    {
+        $this->valutazione = clone $valutazione;
+    }
+
+    /**
+     * Imposta le risposte del thread.
+     * @param array $risposte
+     */
+    public function setRisposte(array $risposte): void
+    {
+        $this->risposte = $risposte;
+    }
     }
--- UniChat/Entity/EValutazione.php
+++ UniChat/Entity/EValutazione.php
@@ -3,77 +3,156 @@
     declare(strict_types = 1);
     require_once __DIR__ . "\..\utility.php";
 
-    class EValutazione
+/**
+ *
+ */
+class EValutazione
     {
 
-        private int $id;
-        private int $totale;
-        private array $utentiPositivi;
-        private array $utentiNegativi;
+    /**
+     * Identificativo della valutazione del thread.
+     * @var int
+     */
+    private int $id;
 
-        public function __construct(int $id, ?int $totale, ?array $utentiPositivi, ?array $utentiNegativi)
-        {
+    /**
+     * Valore della valutazione del thread.
+     * @var int
+     */
+    private int $totale;
+
+    /**
+     * Array contenente gli utenti che hanno valutato positivamente il thread.
+     * @var array
+     */
+    private array $utentiPositivi;
+
+    /**
+     * Array contenente gli utenti che hanno valutato negativamente il thread.
+     * @var array
+     */
+    private array $utentiNegativi;
+
+    /**
+     * Se la valutazione è stata appena aggiunta al thread allora non possiede un id, viene passato come null e settato
+     * a 0.
+     * Quando la valutazione viene appena aggiunta ha valore 0, il totale viene passato come null.
+     * Quando la valutazione viene appena aggiunta non ha ancora utenti che hanno espresso il proprio giudizio, viene
+     * passato null e settati i due array come vuoti.
+     * @param int|null $id
+     * @param int|null $totale
+     * @param array|null $utentiPositivi
+     * @param array|null $utentiNegativi
+     */
+    public function __construct(?int $id, ?int $totale, ?array $utentiPositivi, ?array $utentiNegativi)
+    {
+        if(isset($id)) {
             $this->id = $id;
-            if(isset($totale)){
-                $this->totale = $totale;
-            } else {
-                $this->totale = 0;
-            }
-            if(isset($utentiPositivi)){
-                $this->utentiPositivi = $utentiPositivi;
-            } else {
-                $this->utentiPositivi = array();
-            }
-            if(isset($utentiNegativi)){
-                $this->utentiNegativi = $utentiNegativi;
-            } else {
-                $this->utentiNegativi = array();
-            }
+        } else {
+            $this->id = 0;
         }
+        if(isset($totale)){
+            $this->totale = $totale;
+        } else {
+            $this->totale = 0;
+        }
+        if(isset($utentiPositivi)){
+            $this->utentiPositivi = $utentiPositivi;
+        } else {
+            $this->utentiPositivi = array();
+        }
+        if(isset($utentiNegativi)){
+            $this->utentiNegativi = $utentiNegativi;
+        } else {
+            $this->utentiNegativi = array();
+        }
+    }
 
-        /**
-         * @return int
-         */
-        public function getId(): int
-        {
-            return $this->id;
-        }
+    /**
+     * Restituisce l'identificativo della valutazione del thread.
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
 
-        /**
-         * @return int
-         */
-        public function getTotale(): int
-        {
-            return $this->totale;
-        }
+    /**
+     * Restituisce il valore della valutazione.
+     * @return int
+     */
+    public function getTotale(): int
+    {
+        return $this->totale;
+    }
 
-        /**
-         * @return array
-         */
-        public function getUtentiPositivi(): array
-        {
-            return $this->utentiPositivi;
-        }
+    /**
+     * Restituisce l'array contenente gli utenti che hanno espresso un giudzio positivo.
+     * @return array
+     */
+    public function getUtentiPositivi(): array
+    {
+        return $this->utentiPositivi;
+    }
 
-        /**
-         * @return array
-         */
-        public function getUtentiNegativi(): array
-        {
-            return $this->utentiNegativi;
-        }
+    /**
+     * Restituisce l'array contenente gli utenti che hanno espresso un giudizio negativo.
+     * @return array
+     */
+    public function getUtentiNegativi(): array
+    {
+        return $this->utentiNegativi;
+    }
 
-        /**
-         * @return void
-         */
-        public function valuta(EUser $user, int $valore): void
-        {
-            if($valore == 1){
-                $this->totale = $this->totale + 1;
-                $this->utentiPositivi[] = $user;
-            } else if ($valore == -1){
-                $this->totale = $this->totale - 1;
-                $this->utentiNegativi[] = $user;
-            }
+    /**
+     * Imposta l'identificativo della valutazione.
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * Imposta il totale della valutazione.
+     * @param int $totale
+     */
+    public function setTotale(int $totale): void
+    {
+        $this->totale = $totale;
+    }
+
+    /**
+     * Imposta l'array contenente gli utenti che hanno espresso un giudizio positivo.
+     * @param array $utentiPositivi
+     */
+    public function setUtentiPositivi(array $utentiPositivi): void
+    {
+        $this->utentiPositivi = $utentiPositivi;
+    }
+
+    /**
+     * Imposta l'array contenente gli utenti che hanno espresso un giudizio negativo.
+     * @param array $utentiNegativi
+     */
+    public function setUtentiNegativi(array $utentiNegativi): void
+    {
+        $this->utentiNegativi = $utentiNegativi;
+    }
+
+
+    /**
+     * @param EUser $user
+     * @param int $valore
+     */
+    public function valuta(EUser $user, int $valore): void
+    {
+        if($valore == 1){
+            $this->totale = $this->totale + 1;
+            $this->utentiPositivi[] = $user;
+        } else if ($valore == -1){
+            $this->totale = $this->totale - 1;
+            $this->utentiNegativi[] = $user;
         }
+    }
     }
