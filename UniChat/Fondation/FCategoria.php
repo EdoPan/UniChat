3af5c87dde04981a9b19fd12a53aa76f49b8bbb1
Nino Giuseppe Critelli <critellinino@gmail.com>
2021-08-24 00:57:01 +0200

    Aggiornate classi Foundation FCategoria e FMessaggio (fino a loadUltimiMessaggi)

--- UniChat/Fondation/FCategoria.php
+++ UniChat/Fondation/FCategoria.php
@@ -54,6 +54,34 @@
         }
 
         /**
+         * Recupero icona categoria.
+         * @param int $iconaID
+         * @return array|null
+         */
+
+        public static function loadIcona (int $iconaID): ?array
+        {
+
+            $pdo=new PDO ("mysql:host=localhost, dbname=testing", "root", "pippo");
+            $stmt=$pdo->query("SELECT * FROM categorie AS c, icona AS i WHERE c.iconaID=i.iconaID AND i.iconaID=".$iconaID);
+            $rows=$stmt->fetchAll(PDO::FETCH_ASSOC);
+            if (count($rows)==1) {
+                $record=$rows[0];
+                $icona=array();
+                $icona[0]=(int)$record["iconaID"];
+                $icona[1]=$record["nome"];
+                $icona[2]=$record["dimensione"];
+                $icona[3]=$record["tipo"];
+                $icona[4]=["immagine"];
+
+                return $icona;
+            }
+            else {
+                return null;
+            }
+        }
+
+        /**
          * Recupero di una categoria di un Thread dal proprio ID.
          * @param int $threadID
          * @return ECategoria|null
--- UniChat/Fondation/FMessaggio.php
+++ UniChat/Fondation/FMessaggio.php
@@ -3,14 +3,76 @@
 declare(strict_types = 1);
 require_once __DIR__ . "\..\utility.php";
 
-class FMessaggio
-{
-    public static function delete(int $messID): bool
+/**
+ * Classe Foundation di Messaggio. Conosce la tabella messaggi.
+ */
+    class FMessaggio
     {
-        $pdo = new PDO ("mysql:host=localhost;dbname=testing", "root", "pippo");
-        $sql = ("DELETE FROM messaggi WHERE messID = " . $messID);
-        $stmt = $pdo->prepare($sql);
-        $result = $stmt->execute();
-        return $result;
+        /**
+         * Recupero di un Messaggio da DB.
+         * @param int $messaggioID
+         * @return EMessaggio|null
+         */
+        public static function load(int $messaggioID): ?EMessaggio
+        {
+            $pdo = new PDO ("mysql:host=localhost;dbname=testing", "root", "pippo");
+            $stmt = $pdo->query("SELECT * FROM messaggi WHERE messID = " . $messaggioID);
+            $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            if (count($rows) == 1) {
+                $record = $rows[0];
+                $messID = (int)$record["messID"];
+                $autore = $record["autoreMessID"];
+                $testo = $record["testo"];
+                $data = $record["data"];
+                $messaggio = new EMessaggio($messID, $testo, $autore);
+
+                return $messaggio;
+            } else {
+                return null;
+            }
+        }
+
+
+        public static function loadUltimiMessaggi(int $numero): Array
+
+        {
+
+            $pdo = new PDO ("mysql:host=localhost;dbname=testing", "root", "pippo");
+            $stmt=$pdo->query("SELECT * FROM messaggi ORDER BY messID DESC" );
+            $rows=$stmt->fetchAll(PDO::FETCH_ASSOC);
+            $messaggi=array();
+            for ($i=0; $i++; $i< $numero) {
+                $record =$rows[$i];
+                $messID = (int)$record["messID"];
+                $autore = $record["autoreMessID"];
+                $testo = $record["testo"];
+                $data = $record["data"];
+                $messaggio = new EMessaggio($messID, $testo, $autore);
+                $messaggi[]=$messaggio;
+            }
+
+            return $messaggi;
+
+        }
+
+
+
+
+
+
+
+
+        /**
+         * Rimozione messaggio da DB.
+         * @param int $messID
+         * @return bool
+         */
+        public static function delete(int $messID): bool
+        {
+            $pdo = new PDO ("mysql:host=localhost;dbname=testing", "root", "pippo");
+            $sql = ("DELETE FROM messaggi WHERE messID = " . $messID);
+            $stmt = $pdo->prepare($sql);
+            $result = $stmt->execute();
+            return $result;
+        }
     }
-}
