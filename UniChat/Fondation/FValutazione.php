566e8e7f3cd43d987afbf51de7a63f18e21e03f3
AnToN1o99 <antonio.diluca1999@gmail.com>
2021-08-25 17:38:16 +0200

    Correzione metodi load, update, store, storeVotiPositivi, storeVotiNegativi.

--- UniChat/Fondation/FValutazione.php
+++ UniChat/Fondation/FValutazione.php
@@ -5,28 +5,172 @@
 
     class FValutazione
     {
-        public static function getLastID(): int
+        private static $instance = null;
+
+        private function __construct() {}
+
+        public static function getInstance(): FValutazione
         {
-            $pdo = new PDO ("mysql:host=localhost;dbname=testing", "root", "pippo");
-            $stmt = $pdo->query("SELECT MAX(valutazioneID) AS id FROM valutazioni");
-            $row = $stmt->fetchAll(PDO::FETCH_ASSOC);
-            return (int)$row[0]["id"];
+            $classe = __CLASS__;
+            if (self::$instance == null) {
+                self::$instance == new $classe;
+            }
+            return self::$instance;
         }
 
-        public static function store(EValutazione $v): bool
+        public function load(int $valutazioneID): ?EValutazione
         {
-            $valutazioneID = $v->getId();
-            $totale = $v->getTotale();
-            $pdo = new PDO ("mysql:host=localhost;dbname=testing", "root", "pippo");
-            $sql = ("INSERT INTO valutazioni(valutazioneID, totale) VALUES (:valutazioneID, :totale)");
+            try {
+                $dbConnection = FConnection::getInstance();
+                $pdo = $dbConnection->connect();
+
+                $query1 = ("SELECT totale FROM valutazioni WHERE valutazioneID = :valutazioneID");
+                $stmt = $pdo->prepare($query1);
+                $stmt->execute(array(
+                    ':valutazioneID' => $valutazioneID
+                ));
+
+                $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                $totale = $rows[0]["totale"];
+
+                $query2 = ("SELECT * FROM valutazioni, votipositivi WHERE valutazioni.valutazioneID = votipositivi.valutazioneID AND valutazioni.valutazioneID = :valutazioneID");
+                $stmt = $pdo->prepare($query2);
+                $stmt->execute(array(
+                    ":valutazioneID" => $valutazioneID
+                ));
+                $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                $utentiPositivi = array();
+                foreach ($rows as $row) {
+                    $userID = $row["userID"];
+                    $fThread = FThread::getInstance();
+                    $user = $fThread->load($userID);
+                    if (isset($user)) {
+                        $utentiPositivi[] = $user;
+                    } else {
+                        return null;
+                    }
+                }
+
+                $query3 = ("SELECT * FROM valutazioni, votinegativi WHERE valutazioni.valutazioneID = votinegativi.valutazioneID AND valutazioni.valutazioneID = :valutazioneID");
+                $stmt = $pdo->prepare($query3);
+                $stmt->execute(array(
+                    ":valutazioneID" => $valutazioneID
+                ));
+                $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                $utentiNegativi = array();
+                foreach ($rows as $row) {
+                    $userID = $row["userID"];
+                    $fThread = FThread::getInstance();
+                    $user = $fThread->load($userID);
+                    if (isset($user)) {
+                        $utentiNegativi[] = $user;
+                    } else {
+                        return null;
+                    }
+                }
+
+                $valutazione = new EValutazione($valutazioneID, $totale, $utentiPositivi, $utentiNegativi);
+                return $valutazione;
+            } catch (PDOException $e) {
+                return null;
+            }
+        }
+
+        public function update(EValutazione $valutazione): bool
+        {
+            $valutazioneID = $valutazione->getId();
+            $totale = $valutazione->getTotale();
+            $utentiPositivi = $valutazione->getUtentiPositivi();
+            $utentiNegativi = $valutazione->getUtentiNegativi();
+
+            try {
+                $dbConnection = FConnection::getInstance();
+                $pdo = $dbConnection->connect();
+                $pdo->beginTransaction();
+
+                $resultStoreVotiPositivi = true;
+                $resultStoreVotiNegativi = true;
+
+                if (count($utentiPositivi) > 0) {
+                    $userPositivoID = $utentiPositivi[count($utentiPositivi) - 1]->getId();
+                    if ($this->existsInVotiPositivi($pdo, $valutazioneID, $userPositivoID) == false) {
+                        $resultStoreVotiPositivi = $this->storeVotiPositivi($pdo, $valutazioneID, $userPositivoID);
+                    }
+                }
+
+                if (count($utentiNegativi) > 0) {
+                    $userNegativoID = $utentiNegativi[count($utentiNegativi) - 1]->getId();
+                    if ($this->existsInVotiNegativi($pdo, $valutazioneID, $userNegativoID) == false) {
+                        $resultStoreVotiNegativi = $this->storeVotiNegativi($pdo, $valutazioneID, $userNegativoID);
+                    }
+                }
+
+                $sql = ("UPDATE valutazioni SET totale = :totale WHERE valutazioneID = :valutazioneID");
+                $stmt = $pdo->prepare($sql);
+                $resultUpdateValutazioni = $stmt->execute(array(
+                    ':totale' => $totale,
+                    ':valutazioneID' => $valutazioneID
+                ));
+
+                if ($resultStoreVotiPositivi == true && $resultStoreVotiNegativi == true && $resultUpdateValutazioni == true) {
+                    $pdo->commit();
+                    return true;
+                } else {
+                    $pdo->rollBack();
+                    return false;
+                }
+            } catch (PDOException $e) {
+                return false;
+            }
+        }
+
+        public function store(EValutazione $valutazione): bool
+        {
+            $valutazioneID = $valutazione->getId();
+            $totale = $valutazione->getTotale();
+
+            try {
+                $dbConnection = FConnection::getInstance();
+                $pdo = $dbConnection->connect();
+
+                $sql = ("INSERT INTO valutazioni(valutazioneID, totale) VALUES (:valutazioneID, :totale)");
+                $stmt = $pdo->prepare($sql);
+                $result = $stmt->execute(array(
+                    ':valutazioneID' => $valutazioneID,
+                    ':totale' => $totale
+                ));
+                return $result;
+            } catch (PDOException $e) {
+                return false;
+            }
+        }
+
+        private function storeVotiPositivi(PDO $pdo, int $valutazioneID, int $userID): bool
+        {
+            try {
+                $sql = ("INSERT INTO votipositivi(valutazioneID, userID) VALUES (:valutazioneID, :userID)");
+                $stmt = $pdo->prepare($sql);
+                $result = $stmt->execute(array(
+                    ':valutazioneID' => $valutazioneID,
+                    ':userID' => $userID
+                ));
+                return $result;
+            } catch (PDOException $e) {
+                return false;
+            }
+        }
+
+        private function storeVotiNegativi(PDO $pdo, int $valutazioneID, $userID)
+        {
+            $sql = ("INSERT INTO votinegativi(valutazioneID, userID) VALUES (:valutazioneID, :userID)");
             $stmt = $pdo->prepare($sql);
             $result = $stmt->execute(array(
                 ':valutazioneID' =>  $valutazioneID,
-                ':totale' => $totale
+                ':userID' => $userID
             ));
             return $result;
         }
-
+/*
         public static function loadValutazioneThread(int $threadID): EValutazione
         {
             $pdo = new PDO ("mysql:host=localhost;dbname=testing", "root", "pippo");
@@ -56,39 +200,10 @@
             $valutazione = new EValutazione($valutazioneID, $totale, $utentiPositivi, $utentiNegativi);
             return $valutazione;
         }
+*/
 
-        public static function update(EValutazione $valutazione): bool
+        private function existsInVotiPositivi(PDO $pdo, int $valutazioneID, int $userID): bool
         {
-            $valutazioneID = $valutazione->getId();
-            $totale = $valutazione->getTotale();
-            $utentiPositivi = $valutazione->getUtentiPositivi();
-            $utentiNegativi = $valutazione->getUtentiNegativi();
-            if(count($utentiPositivi) > 0){
-                $userPositivoID = $utentiPositivi[count($utentiPositivi) - 1]->getId();
-                if(self::existsInVotiPositivi($valutazioneID, $userPositivoID) == false){
-                    self::storeVotiPositivi($valutazioneID, $userPositivoID);
-                }
-
-            }
-            if(count($utentiNegativi) > 0){
-                $userNegativoID = $utentiNegativi[count($utentiNegativi) - 1]->getId();
-                if (self::existsInVotiNegativi($valutazioneID, $userNegativoID) == false){
-                    self::storeVotiNegativi($valutazioneID, $userNegativoID);
-                }
-            }
-            $pdo = new PDO ("mysql:host=localhost;dbname=testing", "root", "pippo");
-            $sql = ("UPDATE valutazioni SET totale = :totale WHERE valutazioneID = :valutazioneID");
-            $stmt = $pdo->prepare($sql);
-            $result = $stmt->execute(array(
-                ':totale' =>  $totale,
-                ':valutazioneID' => $valutazioneID
-            ));
-            return $result;
-        }
-
-        private static function existsInVotiPositivi(int $valutazioneID, int $userID): bool
-        {
-            $pdo = new PDO ("mysql:host=localhost;dbname=testing", "root", "pippo");
             $stmt = $pdo->query("SELECT * FROM votipositivi WHERE userID=" . $userID . " AND valutazioneID=" . $valutazioneID);
             $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
             if (count($rows) == 1) {
@@ -98,9 +213,8 @@
             }
         }
 
-        private static function existsInVotiNegativi(int $valutazioneID, int $userID): bool
+        private function existsInVotiNegativi(PDO $pdo, int $valutazioneID, int $userID): bool
         {
-            $pdo = new PDO ("mysql:host=localhost;dbname=testing", "root", "pippo");
             $stmt = $pdo->query("SELECT * FROM votinegativi WHERE userID=" . $userID . " AND valutazioneID=" . $valutazioneID);
             $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
             if (count($rows) == 1) {
@@ -108,30 +222,6 @@
             } else {
                 return false;
             }
-        }
-
-        private static function storeVotiPositivi(int $valutazioneID, int $userID): bool
-        {
-            $pdo = new PDO ("mysql:host=localhost;dbname=testing", "root", "pippo");
-            $sql = ("INSERT INTO votipositivi(valutazioneID, userID) VALUES (:valutazioneID, :userID)");
-            $stmt = $pdo->prepare($sql);
-            $result = $stmt->execute(array(
-                ':valutazioneID' =>  $valutazioneID,
-                ':userID' => $userID
-            ));
-            return $result;
-        }
-
-        private static function storeVotiNegativi(int $valutazioneID, $userID)
-        {
-            $pdo = new PDO ("mysql:host=localhost;dbname=testing", "root", "pippo");
-            $sql = ("INSERT INTO votinegativi(valutazioneID, userID) VALUES (:valutazioneID, :userID)");
-            $stmt = $pdo->prepare($sql);
-            $result = $stmt->execute(array(
-                ':valutazioneID' =>  $valutazioneID,
-                ':userID' => $userID
-            ));
-            return $result;
         }
 
         public static function deleteByThread(int $threadID): bool
