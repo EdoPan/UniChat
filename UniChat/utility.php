1bd0833c5f4fb1daa4d5e54c192c5360576fc083
AnToN1o99 <antonio.diluca1999@gmail.com>
2021-09-02 18:10:59 +0200

    Aggiunta gestione del nuovo path per l'autoload.

--- UniChat/utility.php
+++ UniChat/utility.php
@@ -4,12 +4,15 @@
         $entityClass = __DIR__ . "/" . "Entity" . "/" . $className . ".php";
         $fondationClass = __DIR__ . "/" . "Fondation" . "/" . $className . ".php";
         $controlClass = __DIR__ . "/" . "Control" . "/" . $className . ".php";
+        $viewUtilityClass = __DIR__ . "/" . "View" . "/" . "Utility" . "/" . $className . ".php";
         if(file_exists($entityClass)){
             include $entityClass;
         } else if (file_exists($fondationClass)) {
             include $fondationClass;
         } else if (file_exists($controlClass)){
-            include  $controlClass;
+            include $controlClass;
+        } else if (file_exists($viewUtilityClass)) {
+            include $viewUtilityClass;
         } else {
             return false;
         }
