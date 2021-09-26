403c8cf1e630a397a1e47e2de9a571d5ff02f7af
AnToN1o99 <antonio.diluca1999@gmail.com>
2021-09-26 12:14:05 +0200

    Aggiunto percorso per recuperare classi dal percorso /Entity/Utility.

--- UniChat/utility.php
+++ UniChat/utility.php
@@ -2,17 +2,20 @@
     function autoload($className)
     {
         $entityClass = __DIR__ . "/" . "Entity" . "/" . $className . ".php";
+        $entityUtilityClass = __DIR__ . "/" . "Entity" . "/" . "Utility" . "/" . $className . ".php";
         $fondationClass = __DIR__ . "/" . "Fondation" . "/" . $className . ".php";
         $controlClass = __DIR__ . "/" . "Control" . "/" . $className . ".php";
-        $viewUtilityClass = __DIR__ . "/" . "View" . "/" . "Utility" . "/" . $className . ".php";
+        $viewClass = __DIR__ . "/" . "View" . "/" . $className . ".php";
         if(file_exists($entityClass)){
             include $entityClass;
+        } else if (file_exists($entityUtilityClass)) {
+            include $entityUtilityClass;
         } else if (file_exists($fondationClass)) {
             include $fondationClass;
         } else if (file_exists($controlClass)){
             include $controlClass;
-        } else if (file_exists($viewUtilityClass)) {
-            include $viewUtilityClass;
+        } else if (file_exists($viewClass)) {
+            include $viewClass;
         } else {
             return false;
         }
