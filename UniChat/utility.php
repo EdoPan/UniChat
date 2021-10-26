4742617c9d93f1f52e7334393ab3f70a433ea5c5
AnToN1o99 <antonio.diluca1999@gmail.com>
2021-10-26 15:01:24 +0200

    Modifiche.

--- UniChat/utility.php
+++ UniChat/utility.php
@@ -4,6 +4,7 @@
         $entityClass = __DIR__ . "/" . "Entity" . "/" . $className . ".php";
         $entityUtilityClass = __DIR__ . "/" . "Entity" . "/" . "Utility" . "/" . $className . ".php";
         $fondationClass = __DIR__ . "/" . "Fondation" . "/" . $className . ".php";
+        $fondationUtilityClass = __DIR__ . "/" . "Fondation" . "/" . "Utility" . "/" . $className . ".php";
         $controlClass = __DIR__ . "/" . "Control" . "/" . $className . ".php";
         $viewClass = __DIR__ . "/" . "View" . "/" . $className . ".php";
         if(file_exists($entityClass)){
@@ -12,6 +13,8 @@
             include $entityUtilityClass;
         } else if (file_exists($fondationClass)) {
             include $fondationClass;
+        } else if (file_exists($fondationUtilityClass)) {
+            include $fondationUtilityClass;
         } else if (file_exists($controlClass)){
             include $controlClass;
         } else if (file_exists($viewClass)) {
